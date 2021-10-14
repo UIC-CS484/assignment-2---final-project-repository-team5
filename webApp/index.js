21cee2cdcd5afc112a8f220dfc7c3f89b3b43e8e
Sahil Gedam <sahil.gedam15@gmail.com>
2021-10-14 15:39:26 -0500

    Trying to set up router

--- webApp/index.js
+++ webApp/index.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+
+//middleware
+
+//handlebars
 app.set('views', './views');
 app.set('view engine', 'hbs');
+
+//parse application/json
+app.use(express.json());
+
+app.use(express.urlencoded());
 
 app.get('/',(req, res) => {
     res.render('index');

5252d90ed1ece5e90554dfc7b43efbc98d6be3e9
Annsh Agrawaal <aagraw26@uic.edu>
2021-10-14 11:39:16 -0500

    Added all paths and routes

--- webApp/index.js
+++ webApp/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const createError = require('http-errors');
 
 app.set('views', './views');
 app.set('view engine', 'hbs');
@@ -9,12 +10,20 @@ app.get('/',(req, res) => {
     res.render('index');
 })
 
-// app.post('/info', (req, res) =>{
-//     const lastName = req.body.lname;
-//     const firstName = req.body.fname;
-//     console.log(lastName + " , " + firstName );
-//     res.send(`<ul> <li>First Name: ${firstName}</li>  <li>Last Name: ${lastName}</li></ul>`);
-// })
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//     next(createError(404));
+// });
+
+const loginRouter = require('./routes/loginPage')
+const createAccountRouter = require('./routes/createAccountPage')
+const updateAccountRouter = require('./routes/updateAccountPage')
+const deleteAccountRouter = require('./routes/deleteAccountPage')
+
+app.use('/',loginRouter)
+app.use('/createaccount',createAccountRouter)
+app.use('/updateaccount',updateAccountRouter)
+app.use('/deleteaccount',deleteAccountRouter)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)

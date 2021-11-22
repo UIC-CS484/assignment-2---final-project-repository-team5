023e343adf3ccddae18040c436593924222c53ff
Sahil Gedam <sahil.gedam15@gmail.com>
2021-11-22 14:54:48 -0600

    Added basic sql database

--- webApp/index.js
+++ webApp/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 const createError = require('http-errors');
+const session = require('express-session');
+const SQLiteStore = require('connect-sqlite3')(session);
+const sqlite3 = require('sqlite3').verbose() //npm install sqlite3
 
 app.set('views', './views');
 app.set('view engine', 'hbs');
@@ -17,6 +20,7 @@ const createAccountRouter = require('./routes/createAccountPage')
 const updateAccountRouter = require('./routes/updateAccountPage')
 const deleteAccountRouter = require('./routes/deleteAccountPage')
 const errorAccountRouter = require('./routes/errorAccountPage')
+const afterLoggingInRouter = require('./routes/loginFormSubmit')
 
 // Linking routes to routers.
 app.use('/login',loginRouter)
@@ -24,6 +28,7 @@ app.use('/createAccount',createAccountRouter)
 app.use('/updateAccount',updateAccountRouter)
 app.use('/deleteAccount',deleteAccountRouter)
 app.use('/error',errorAccountRouter)
+app.use('/afterLoggingIn', afterLoggingInRouter)
 
 // Render home.hbs view
 app.get('/',(req, res) => {
@@ -39,7 +44,7 @@ app.get('/login',(req, res) => {
 
 // Render createAccount.hbs view
 app.get('/createAccount', (req,res) => {
-    res.render('createAccount')
+    //res.render('createAccount')
     console.log("/createAccount called")
 })
 
@@ -50,13 +55,15 @@ app.get('/updateAccount', (req,res) => {
 })
 
 // Render deleteAccount.hbs view
-app.get('deleteAccount', (req,res) => {
+app.get('/deleteAccount', (req,res) => {
     res.render('deleteAccount')
     console.log("/deleteAccount called")
 })
 
 // Login Form Submitted
-app.get('/loginFormSubmit',(req, res)=>{
+app.get('/afterLoggingIn',(req, res)=>{
+    //console.log("here");
+    //res.render('afterLoggingIn')
     console.log(req.query.username, req.query.password)
     //authenticate, re-route on success and failure
 })
@@ -65,3 +72,4 @@ app.get('/loginFormSubmit',(req, res)=>{
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+

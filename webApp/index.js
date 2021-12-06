9ba92fb09c4f1f94955852fe558a96f058e3a2b0
Annsh Agrawaal <agrawaalannsh@gmail.com>
2021-12-06 12:13:42 -0600

    Implemented 2nd API
    Number of times played per song console log

--- webApp/index.js
+++ webApp/index.js
@@ -81,49 +81,46 @@ app.get('/results',(req, res)=>{
         }
     };
 
-    const options2 = {
-        method: 'GET',
-        url: 'https://shazam.p.rapidapi.com/songs/get-count',
-        params: {key: id},
-        headers: {
-            'x-rapidapi-host': 'shazam.p.rapidapi.com',
-            'x-rapidapi-key': '7ae14268e5msh241a0d6655ae5fdp1aaa57jsnefdf711c7928'
-        }
-    };
-//////////////////////////
-    class Example {
-        async asyncMethod() {
-            const data = await axios.request(options);
-            return data
-        }
-    }
-
-    const exampleClass = new Example();
-    exampleClass.asyncMethod().then((response) => {
-        id = response.tracks.hits.[0].track.key;
-        return axios.request(options2)
-    }).then((response) => {
-        res.render('results', {data:response.data})
-    }).catch(function (error) {
-        console.error(error);
-
-    axios.request(options).then((response) => {
-            return axios.get(...); // using response.data
-        })
-        .then((response) => {
-            res.render('results', {data:response.data})
-        }).catch(function (error) {
-        console.error(error);
-    });
-//////////////////////////////////////////
-    axios.request(options).then(function (response) {
+    axios.request(options).then(async function (response) {
         // console.log(JSON.stringify(response.data));
-        res.render('results', {data:response.data})
+
+        let responses = []
+        for (let hit of response.data.tracks.hits) {
+            console.log(hit.track.key)
+            const options2 = {
+                method: 'GET',
+                url: 'https://shazam.p.rapidapi.com/songs/get-count',
+                params: {key: hit.track.key},
+                headers: {
+                    'x-rapidapi-host': 'shazam.p.rapidapi.com',
+                    'x-rapidapi-key': '7ae14268e5msh241a0d6655ae5fdp1aaa57jsnefdf711c7928'
+                }
+            };
+
+            responses.push(await axios.request(options2))
+        }
+
+        totals = []
+        for (let res of responses) {
+            // console.log("DATA IS HEREEEE")
+            // console.log(res.data);
+            let obj = {
+                name: response.data.tracks.hits[0].track.title,
+                total: res.data.total
+            }
+            totals.push(obj)
+        }
+
+        console.log(totals)
+        res.render('results', {data:{data1: response.data, data2: totals}})
+
     }).catch(function (error) {
         console.error(error);
     });
 
 })
+
+
 
 app.get('/dashboard',(req, res)=>{
     res.render('dashboard')

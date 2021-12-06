a30021d92d852873b955afbe61eed6808f9e77dc
Aryan Thaman <aryanthaman>
2021-12-06 01:48:54 -0600

    login and createAccount workflow complete

--- webApp/database_function.js
+++ webApp/database_function.js
@@ -1,4 +1,5 @@
 var sqlite3 = require('sqlite3').verbose() //npm install sqlite3
+// const bcrypt = require('bcryptjs');
 
 //Creating a new database instance - Indication of connected database
 //Before performing any operations to database, make sure database is connected.
@@ -14,10 +15,12 @@ let db = new sqlite3.Database('./database/task.sqlite', (err) => {
 });
 
 //Create a User
-let createUser = (id, email, username, password) =>{
-    console.log("HERE")
-    var createUserSql = 'INSERT INTO USER (user_id, user_email, user_name ,user_password) VALUES (?,?,?,?)'
-    var params =[null, email, username, password];
+let createUser = (id, firstName, lastName, username, password) =>{
+    console.log("In createUser")
+
+
+    var createUserSql = 'INSERT INTO USER(user_id, user_firstName, user_lastName, user_userName ,user_userPassword) VALUES (?,?,?,?,?)'
+    var params =[id ,firstName, lastName, username, password];
 
     db.run(createUserSql, params, function(err){
         if (err){
@@ -28,8 +31,41 @@ let createUser = (id, email, username, password) =>{
     });
 }
 
-createUser(1,"sgedam2", "surgeon15","sahil")
-createUser(2, "aagraw26", "bighsot", "anssh")
-createUser(3, "athama2", "kronicrage", "aryan")
+//Return a user from database
+let authenticateUser = (username, password, done) =>{
 
-module.exports = {createUser};
+    var findUser = 'SELECT * FROM USER WHERE user_userName = ?';
+
+    db.get(findUser, username, function (err, user) {
+        //console.log(user);
+        if (!user) {
+            //return done(null, false);
+            console.log("in above if User not found")
+        }
+        console.log("Entered password is: "+ password+ " Original password is: " +user.user_userPassword)
+        if(password === user.user_userPassword){
+            console.log("User does exist")
+            console.log(user)
+            done(null, user)
+        }
+        else{
+            console.log("User does NOT exist")
+            done(null, false)
+        }
+        /*        bcrypt.compare(password, "password" , function (err, result) {
+                    if (err) {
+                        return console.log(err.message);
+                    }
+                    console.log(result)
+                    if (result) {
+                        return done(null, user);
+                    }
+                    else{
+                        console.log("in below if - User not found")
+                    }
+                });*/
+
+    });
+}
+
+module.exports = {db, createUser, authenticateUser}
--- webApp/index.js
+++ webApp/index.js
@@ -1,18 +1,13 @@
 const express = require('express')
-const app = express()
+
 const port = 3000
 const createError = require('http-errors');
 const session = require('express-session');
 const SQLiteStore = require('connect-sqlite3')(session);
 const sqlite3 = require('sqlite3').verbose() //npm install sqlite3
-
-app.set('views', './views');
-app.set('view engine', 'hbs');
-
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//     next(createError(404));
-// });
+const passport = require('passport');
+// var path = require('path');
+const cookieParser = require('cookie-parser');
 
 // Creating routers
 const loginRouter = require('./routes/loginPage')
@@ -20,11 +15,48 @@ const createAccountRouter = require('./routes/createAccountPage')
 const updateAccountRouter = require('./routes/updateAccountPage')
 const deleteAccountRouter = require('./routes/deleteAccountPage')
 const errorAccountRouter = require('./routes/errorAccountPage')
-const afterLoggingInRouter = require('./routes/loginFormSubmit')
+const loginSubmitRouter = require('./routes/loginFormSubmit')
 const dashboardRouter = require('./routes/dashboard')
+const submitRouter = require('./routes/submit');
 const axios = require("axios");
 const {log} = require("nodemon/lib/utils");
 
+const app = express()
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//     next(createError(404));
+// });
+
+//passport config
+require('./config/passport')(passport);
+
+var session_config = {
+    secret: 'secret', //a random unique string key used to authenticate a session
+    resave: true, //enables the session to be stored back to the session store, even if the session was never modified during the request
+    saveUninitialized: true, //his allows any uninitialized session to be sent to the store. When a session is created but not modified, it is referred to as uninitialized.
+    cookie: { secure: true } //true is a recommended option. However, it requires an https-enabled website
+    //store  parameter when saving session to database
+};
+
+session_config.cookie.secure = false;
+//IMPORTANT REVIEW IN CLASS - https://expressjs.com/en/resources/middleware/session.html
+
+//Express Sessions
+app.use(session(session_config))
+//Reference for above - https://www.section.io/engineering-education/session-management-in-nodejs-using-expressjs-and-express-session/
+
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Linking routes to routers.
 app.use('/login',loginRouter)
@@ -32,9 +64,9 @@ app.use('/createAccount',createAccountRouter)
 app.use('/updateAccount',updateAccountRouter)
 app.use('/deleteAccount',deleteAccountRouter)
 app.use('/error',errorAccountRouter)
-app.use('/afterLoggingIn', afterLoggingInRouter)
+app.use('/loginSubmit', loginSubmitRouter)
 app.use('/dashboard',dashboardRouter)
-
+app.use('/submit',submitRouter)
 
 
 // Render home.hbs view
@@ -65,14 +97,6 @@ app.get('/updateAccount', (req,res) => {
 app.get('/deleteAccount', (req,res) => {
     res.render('deleteAccount')
     console.log("/deleteAccount called")
-})
-
-// Login Form Submitted
-app.get('/afterLoggingIn',(req, res)=>{
-    //console.log("here");
-    //res.render('afterLoggingIn')
-    console.log(req.query.username, req.query.password)
-    //authenticate, re-route on success and failure
 })
 
 app.get('/results',(req, res)=>{
